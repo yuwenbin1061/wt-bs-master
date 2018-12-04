b9d4943cfc77ffe6249960efc685f0aeb3d4a5f3
yuwenbin1061 <yuwenbin1061@163.com>
2018-12-05 03:10:05 +0800

    前六个判断

--- wt-bs-service/src/main/java/wt/bs/common/SpecialDialectEnum.java
+++ wt-bs-service/src/main/java/wt/bs/common/SpecialDialectEnum.java
@@ -11,7 +11,7 @@ public enum SpecialDialectEnum {
             List<ChunkType> chunkList = SpecialDialectHandler.sentenceAnalysis(sentence);
 
             for (ChunkType chunk : chunkList){
-                if (SpecialDialectHandler.isAuxiliary(chunk, this.getDialect())){
+                if (SpecialDialectHandler.isAuxiliary(chunk, this.getDialect(), this.getNominal())){
                     return true;
                 }
             }
@@ -21,30 +21,65 @@ public enum SpecialDialectEnum {
     DL02("ひん", "ない（例文：なかなか成績が伸びへん）", "助動詞"){
         @Override
         public Boolean isAnswer(String sentence) {
+            List<ChunkType> chunkList = SpecialDialectHandler.sentenceAnalysis(sentence);
+
+            for (ChunkType chunk : chunkList){
+                if (SpecialDialectHandler.isAuxiliary(chunk, this.getDialect(), this.getNominal())){
+                    return true;
+                }
+            }
             return false;
         }
     },
     DL03("せん", "しない（例文：掃除せんといけない）", "助動詞"){
         @Override
         public Boolean isAnswer(String sentence) {
+            List<ChunkType> chunkList = SpecialDialectHandler.sentenceAnalysis(sentence);
+
+            for (ChunkType chunk : chunkList){
+                if (SpecialDialectHandler.isDL03(chunk)){
+                    return true;
+                }
+            }
             return false;
         }
     },
     DL04("てや", "てね、てよ（例文：見ててや）", "終助詞"){
         @Override
         public Boolean isAnswer(String sentence) {
+            List<ChunkType> chunkList = SpecialDialectHandler.sentenceAnalysis(sentence);
+
+            for (ChunkType chunk : chunkList){
+                if (SpecialDialectHandler.isDL04(chunk)){
+                    return true;
+                }
+            }
             return false;
         }
     },
     DL05("や", "だ（例文：そうや）", "終助詞"){
         @Override
         public Boolean isAnswer(String sentence) {
+            List<ChunkType> chunkList = SpecialDialectHandler.sentenceAnalysis(sentence);
+
+            for (ChunkType chunk : chunkList){
+                if (SpecialDialectHandler.isDL05(chunk)){
+                    return true;
+                }
+            }
             return false;
         }
     },
     DL06("はる","尊敬の表現（例文：先生が食べはる）", "動詞") {
         @Override
         public Boolean isAnswer(String sentence) {
+            List<ChunkType> chunkList = SpecialDialectHandler.sentenceAnalysis(sentence);
+
+            for (ChunkType chunk : chunkList){
+                if (SpecialDialectHandler.isDL06(chunk)){
+                    return true;
+                }
+            }
             return false;
         }
     };
--- wt-bs-service/src/main/java/wt/bs/common/SpecialDialectHandler.java
+++ wt-bs-service/src/main/java/wt/bs/common/SpecialDialectHandler.java
@@ -53,14 +53,83 @@ public class SpecialDialectHandler {
     }
 
     /**
-     * 判断：助動詞
+     * 判断：词性
      */
-    public static boolean isAuxiliary(ChunkType chunk, String dialect){
+    public static boolean isAuxiliary(ChunkType chunk, String dialect, String nominal){
         for (MorphemType morphem : chunk.getMorphemList().getMorphem()){
-            if (morphem.getSurface().equals(dialect) && morphem.getPOS().equals("助動詞")){
+            if (morphem.getSurface().equals(dialect) && morphem.getPOS().equals("nominal")){
                 return true;
             }
         }
         return  false;
     }
+
+    /**
+     * 判断DL03：せん
+     */
+    public static boolean isDL03(ChunkType chunk){
+        int index = 0;
+        for (MorphemType morphem : chunk.getMorphemList().getMorphem()){
+            if (morphem.getSurface().equals("せ") && morphem.getPOS().equals("助動詞")){
+                if ((index+1) < chunk.getMorphemList().getMorphem().size()
+                        && chunk.getMorphemList().getMorphem().get(index+1).getSurface().equals("ん")){
+                    return true;
+                }
+            }
+            index++;
+        }
+        return  false;
+    }
+
+    /**
+     * 判断DL04：てや
+     */
+    public static boolean isDL04(ChunkType chunk){
+        int index = 0;
+        for (MorphemType morphem : chunk.getMorphemList().getMorphem()){
+            if (morphem.getSurface().equals("や") && morphem.getPOS().equals("終助詞")){
+                if ((index - 1) >= 0 && chunk.getMorphemList().getMorphem().get(index - 1).getSurface().equals("て")){
+                    return true;
+                }
+            }
+            index++;
+        }
+        return  false;
+    }
+
+    /**
+     * 判断DL05：や
+     */
+    public static boolean isDL05(ChunkType chunk){
+        int index = 0;
+        for (MorphemType morphem : chunk.getMorphemList().getMorphem()){
+            if (morphem.getSurface().equals("や") && morphem.getPOS().equals("終助詞")){
+                if ((index - 1) >= 0
+                        && (index+1) < chunk.getMorphemList().getMorphem().size()
+                        && !chunk.getMorphemList().getMorphem().get(index - 1).getSurface().equals("て")
+                        &&  chunk.getMorphemList().getMorphem().get(index + 1).getPOS().equals("特殊")){
+                    return true;
+                }
+            }
+            index++;
+        }
+        return  false;
+    }
+
+    /**
+     * 判断DL06：はる
+     */
+    public static boolean isDL06(ChunkType chunk){
+        int index = 0;
+        for (MorphemType morphem : chunk.getMorphemList().getMorphem()){
+            if (morphem.getSurface().equals("はる") && morphem.getPOS().equals("動詞")){
+                if ((index - 1) >= 0 && chunk.getMorphemList().getMorphem().get(index - 1).getPOS().equals("動詞")){
+                    return true;
+                }
+            }
+            index++;
+        }
+        return  false;
+    }
+
 }
